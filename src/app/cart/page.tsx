20eff5740455e517b8fc5b90bd267df2207af322
yuter777 <muhammad06tt@gmail.com>
2024-09-03 18:06:35 +0500

    bu

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,32 +1,43 @@
-"use client"
+"use client";
+
 import { DataType } from "@/types/data.types";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Cart = () => {
-  const data = JSON.parse(`${localStorage.getItem("cart")}`) || [];
+  const [data, setData] = useState<DataType[]>([]);
 
-  return <div>
-    <div className="flex gap-2">
-      <div className="">Savat</div>
-      <div className="">{data.length} tovarlarni</div>
-    </div>
-    <div className="">
-      {
-        data && data?.map((data: DataType)=>(
-          <div className="flex gap-3 py-4 border-b" key={data.id}>
-            <div className="">
-              <Image src={data.thumbnail} alt={data.title} width={100} height={100}/>
+  useEffect(() => {
+    const cartData = JSON.parse(`${localStorage.getItem("cart")}`) || [];
+    setData(cartData);
+  }, []);
+  return (
+    <div>
+      <div className="flex gap-2">
+        <div className="">Savat</div>
+        <div className="">{data.length} tovarlar</div>
+      </div>
+      <div className="">
+        {data &&
+          data.map((item: DataType) => (
+            <div className="flex gap-3 py-4 border-b" key={item.id}>
+              <div className="">
+                <Image
+                  src={item.thumbnail}
+                  alt={item.title}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="">
+                <div className="">{item.title}</div>
+                <div className="">Narx: {item.price}$</div>
+              </div>
             </div>
-            <div className="">
-              <div className="">{data.title}</div>
-              <div className="">Narx: {data.price}$</div>
-            </div>
-          </div>
-        ))
-      }
+          ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Cart;
